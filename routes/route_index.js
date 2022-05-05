837fb1fbfa2b14b4b9b3a914846511227f98dc81
Hana897TRX <nemesisdarkness897gt@gmail.com>
2022-05-04 23:05:41 -0500

    Feature: Login with token

--- routes/route_index.js
+++ routes/route_index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const { token } = require('morgan');
 const router = express.Router();
 const User = require('../model/user');
+const mongoose = require('mongoose')
 
 var app = express();
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var rand = function() {
+  return Math.random().toString(36).substr(2); // remove `0.`
+}
 
+var tokenFs = function() {
+  return rand() + rand(); // to make it longer
+}
 
 router.get('/index', async function(req,res){
 
@@ -24,15 +32,55 @@ router.get('/about', async function(req,res){
   res.render('about');
 });
 
-router.get('/login', async function(req,res){
+router.get('/login', async function(req,res) {
   res.render('login');
 });
 
+router.post('/loginUser', async (req, res) => {
+  let mail = req.body["mail"];
+  let password = req.body["password"];
+
+  if(mail && password) {
+    let response = await User.findOne({ $match: [ { mail: mail }, { password: password } ] })
+    
+    if(response) {
+      let tokenGen = tokenFs();
+      console.log(response["_id"].valueOf())
+      let tokenRs = await User.updateOne({ "_id" : mongoose.Types.ObjectId(response["_id"].valueOf() )}, { token: tokenGen } )
+
+      if(tokenRs) {
+        res.status(200).json({
+          message: "success",
+          code: 200,
+          data: {
+            token: tokenGen
+          }
+        })
+      } else {
+        res.status(500).json({
+          message: "Error, internal error",
+          code: 500,
+          data: []
+        })
+      }
+    } else {
+      res.status(404).json({
+        message: "Error, not match password",
+        code: 404,
+        data: []
+      })
+    }
+  } else {
+    res.status(400).json({
+      message: "Error, no password or mail",
+      code: 400,
+      data: []
+    })
+  }
+})
+
 router.get('/register', async function(req,res){
   res.render('register');
-});
-router.get('/newUser', async (req,res) =>{
-  res.render('newUser');
 });
 
 router.post('/newUser', async (req,res) =>{
